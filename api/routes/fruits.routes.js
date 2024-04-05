1327a31f2b5f65bc4ad38fe8555469d725107e39
Nguyễn Quang Minh (NQM) <nguyenquangminh570@gmaiil.com>
2024-04-05 15:20:50 +0700

    Add search and query routes for fruits

--- api/routes/fruits.routes.js
+++ api/routes/fruits.routes.js
@@ -5,7 +5,6 @@ const fruitsController = require('../controllers/fruits.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 require('dotenv').config();
 const upload = require('../middlewares/multer.middleware');
-const multer = require('multer');
 
 // Create Fruit
 router.post('/create', upload.array("images"), authMiddleware.authenticateToken, fruitsController.createFruit);
@@ -13,11 +12,16 @@ router.post('/create', upload.array("images"), authMiddleware.authenticateToken,
 // Get Fruits
 router.get('/', authMiddleware.authenticateToken, fruitsController.getFruits);
 
+// Search Fruits
+router.get('/search/:name', authMiddleware.authenticateToken, fruitsController.searchFruits);
+
+// Get Fruit using query params
+router.get('/query', authMiddleware.authenticateToken, fruitsController.getFruitByQuery);
+
 // Get Fruit by ID
 router.get('/:fruitId', authMiddleware.authenticateToken, fruitsController.getFruitById);
 
 // Update Fruit by ID
-
 router.patch('/:fruitId', upload.array("images"), authMiddleware.authenticateToken, fruitsController.updateFruit);
 
 // Delete Fruit by ID
